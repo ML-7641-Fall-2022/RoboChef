737db9d90771acac6129c57958ecaef04972f4b5
ASHISH DHIMAN <ashish1610dhiman@gmail.com>
2022-11-12 13:41:43 -0500

    steps

---
+++ src/preprocess.py
@@ -0,0 +1,10 @@
+"""
+# Created by ashish1610dhiman at 11/11/22
+Contact at ashish1610dhiman@gmail.com
+"""
+
+class preprocess():
+    def __init__(self, data):
+        self.data = data
+
+    def serialize(self, data, col):
